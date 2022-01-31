420a3fcbd47e06eb15f6464746cce0688a301324
Gregor Majcen <gregor.majcen@sinergise.com>
2022-01-31 20:12:24 +0100

    tailwind: konstaConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+const konstaConfig = require('konsta/config');
+
+const config = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+module.exports = konstaConfig(config);
