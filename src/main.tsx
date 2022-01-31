7f8c79e8d9d4afef72bc1658df0d7e5bf750ce52
Gregor Majcen <gregor.majcen@sinergise.com>
2022-01-31 20:09:01 +0100

    import roboto fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400-italic.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/500-italic.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/700-italic.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
