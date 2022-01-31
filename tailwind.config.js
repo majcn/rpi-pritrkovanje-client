26a9319764c95fb538121b989a80244ac112fbfd
Gregor Majcen <gregor.majcen@sinergise.com>
2022-01-31 20:12:48 +0100

    trying to solve long titles...

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ const config = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
