20114498f985b04044de4cbb07931d8ff76ef6bd
Gregor Majcen <gregor.majcen@sinergise.com>
2022-01-31 20:11:34 +0100

    use mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,58 @@
-import { useState } from 'react';
-import { Container, MainLayout } from './Components/Layout';
+import { useState, useEffect } from 'react';
 import Music from './Components/Music';
-import Sidebar from './Components/Siderbar';
+
+import AbcPlayer from './MusicPlayer/AbcPlayer';
+import MobileLayout from './Components/Mobile/Layout';
 
 function App() {
-  const zvonovi = [
-    useState(true),
-    useState(true),
-    useState(true),
-    useState(true),
-  ];
+  const [bell1, setBell1] = useState(true);
+  const [bell2, setBell2] = useState(true);
+  const [bell3, setBell3] = useState(true);
+  const [bell4, setBell4] = useState(true);
 
-  const items = [
-    { title: 'Prvi zvon', active: zvonovi[0][0] },
-    { title: 'Drugi zvon', active: zvonovi[1][0] },
-    { title: 'Tretji zvon', active: zvonovi[2][0] },
-    { title: 'Četrti zvon', active: zvonovi[3][0] },
-  ];
+  const bells = [bell1, bell2, bell3, bell4];
+  const bellSetters = [setBell1, setBell2, setBell3, setBell4];
 
-  const handleSidebarClick = (item: string) => {
-    const i = items.findIndex((x) => x.title === item);
-    const [z, s] = zvonovi[i];
-    s(!z);
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
+
+  const toggleMusicPlaying = () => {
+    if (isMusicPlaying) {
+      AbcPlayer.stop();
+      setIsMusicPlaying(false);
+    } else {
+      AbcPlayer.play();
+      setIsMusicPlaying(true);
+    }
   };
 
-  return (
-    <MainLayout>
-      <Container>
-        <Music items={items} songAbcUrl="/api/state/song.abc" />
-      </Container>
+  const [tempo, setTempo] = useState(0);
+  const [title, setTitle] = useState('');
 
-      <Sidebar items={items} onItemClick={handleSidebarClick} />
-    </MainLayout>
+  useEffect(() => {
+    if (tempo > 0) {
+      AbcPlayer.setTempo(tempo);
+    }
+  }, [tempo]);
+
+  return (
+    <MobileLayout
+      theme="ios"
+      server={false}
+      title={title}
+      bells={bells}
+      onBellClick={(i) => bellSetters[i](!bells[i])}
+      bpm={tempo}
+      setBpm={setTempo}
+      isMusicPlaying={isMusicPlaying}
+      toggleMusicPlaying={toggleMusicPlaying}
+    >
+      <Music
+        bells={bells}
+        setTempo={setTempo}
+        setTitle={setTitle}
+        songAbcUrl="/api/state/song.abc"
+      />
+    </MobileLayout>
   );
 }
 
